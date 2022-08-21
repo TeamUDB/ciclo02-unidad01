63ca161de5b52e67b99c37555489cda6456dc111
Victor Cornejo <rex2002xp@gmail.com>
2022-08-20 20:27:44 -0600

    Se agrega la validacion si existe el carnet

--- src/ciclo02/TareaForo.java
+++ src/ciclo02/TareaForo.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author gdanm
@@ -6,27 +7,25 @@
 /*Let's start with the students registration
  *First, and very important, do not forget import
  *the classes to be used along the program
-*/
+ */
 import java.util.*;
-        
-public class TareaForo
-{
-    public static void main(String[] args) 
-    {
+
+public class TareaForo {
+
+    public static void main(String[] args) {
         //Declaring some variables
         String name = "";
         String carnet = "";
         String src_carnet, reg_carnet;
         int key = 0;
-        
+
         //Calling the scanner
-        Scanner input = new Scanner (System.in);
-        
+        Scanner input = new Scanner(System.in);
+
         //Getting hashmap ready
-        HashMap <String, String> stdatabase = new HashMap <String, String> ();
-        
-        do
-        {  
+        HashMap<String, String> stdatabase = new HashMap<String, String>();
+
+        do {
             //Displaying menu
             System.out.println("Bienvenido a UDB Virtual");
             System.out.println("1. Registrar estudiante");
@@ -35,113 +34,99 @@ public class TareaForo
             System.out.println("4. Eliminar estudiante");
             System.out.println("5. Salir");
             System.out.print("Seleccione una opción: ");
-            
+
             //Giving options with switch case structure
             int option = input.nextInt();
             String jumpline = input.nextLine();
-            
-            switch (option)
-            {
-                case 1:
-                {
+
+            switch (option) {
+                case 1: {
                     //Initiating validation, if no name, no pass
-                    while(name.isEmpty())
-                    {
+                    while (name.isEmpty()) {
                         System.out.println("Por favor ingrese nombre completo de estudiante: ");
                         name = input.nextLine();
-                    
-                        if (name.isEmpty())
-                        {
+
+                        if (name.isEmpty()) {
                             System.out.println("Nombre de estudiante es requerido para continuar");
                             System.out.println("\n");
                         }
-                        
+
                     }
-                    
+
                     //Initiating validation, if no ID, no pass
-                    while(carnet.isEmpty())
-                    {
+                    while (carnet.isEmpty()) {
                         System.out.println("Por favor ingrese el carnet del estudiante: ");
                         carnet = input.nextLine();
-                        reg_carnet = stdatabase.get(carnet);
-                        if (carnet.isEmpty())
-                        {
+
+                        if (carnet.isEmpty()) {
                             System.out.println("Carnet es requerido para continuar");
                             System.out.println("\n");
+                        } else {
+                            if (stdatabase.containsKey(carnet)) {
+                                System.out.println("Carnet ya existe.");
+                                System.out.println("\n");
+                                carnet = "";
+                            }
                         }
                     }
-                    
+
                     stdatabase.put(carnet, name);
                     System.out.println("Alumno registrado exitosamente");
+                    name = "";
+                    carnet = "";
                 }
                 break;
-                
-                case 2:
-                {
+
+                case 2: {
                     System.out.print("Por favor escriba el numero de carnet del estudiante: ");
                     src_carnet = input.nextLine();
                     reg_carnet = stdatabase.get(src_carnet);
-                    
-                    if (stdatabase.containsValue(reg_carnet))
-                    {
+
+                    if (stdatabase.containsValue(reg_carnet)) {
                         System.out.println(src_carnet + " - " + reg_carnet);
-                    }
-                    else
-                    {
+                    } else {
                         System.out.println("Alumno no encontrado, no se puede Mostrar");
                     }
                 }
                 break;
-                
-                case 3:
-                {
-                    if (!stdatabase.isEmpty())
-                    {
-                        for (Map.Entry <String, String> map_show : stdatabase.entrySet())
-                        {
+
+                case 3: {
+                    if (!stdatabase.isEmpty()) {
+                        for (Map.Entry<String, String> map_show : stdatabase.entrySet()) {
                             System.out.println(map_show.getKey() + "- " + map_show.getValue());
                         }
-                    }
-                    else
-                    {
+                    } else {
                         System.out.println("No hay alumnos registrados");
                     }
                 }
                 break;
-                
-                case 4:
-                {
+
+                case 4: {
                     System.out.println("Por favor escriba el numero de carnet del estudiante: ");
                     src_carnet = input.nextLine();
                     reg_carnet = stdatabase.get(src_carnet);
-                    
-                    if(stdatabase.containsValue(reg_carnet))
-                    {
+
+                    if (stdatabase.containsValue(reg_carnet)) {
                         stdatabase.remove(src_carnet);
                         System.out.println("Alumno eliminado");
-                    }
-                    else
-                    {
+                    } else {
                         System.out.println("Alumno no encontrado, no se puede Mostrar");
                     }
                 }
                 break;
-                
-                case 5:
-                {
+
+                case 5: {
                     key = 5;
                     System.out.println("Thanks for using our program!");
                     System.exit(0);
                 }
                 break;
-                
-                default:
-                {
+
+                default: {
                     System.out.println("Opcion invalida");
                 }
             }
-        }
-        while (key == 0);
+        } while (key == 0);
     }
-    
+
 }
